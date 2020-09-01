12d169b0df4f2e31303cf49fb3f0b084e7c3e9ae
Alexander Lohnau <alexander.lohnau@gmx.de>
2020-09-01 13:09:21 +0200

    Consider XDG_DATA_HOME env variable for template install dirs

--- templates/runnerpython/install.sh
+++ templates/runnerpython/install.sh
@@ -3,11 +3,18 @@
 # Exit if something fails
 set -e
 
-mkdir -p ~/.local/share/kservices5/krunner/dbusplugins/
-mkdir -p ~/.local/share/dbus-1/services/
 
-cp plasma-runner-%{APPNAMELC}.desktop ~/.local/share/kservices5/krunner/dbusplugins/
-sed "s|%{PROJECTDIR}/%{APPNAMELC}.py|${PWD}/%{APPNAMELC}.py|" "org.kde.%{APPNAMELC}.service" > ~/.local/share/dbus-1/services/org.kde.%{APPNAMELC}.service
+if [[ -z "$XDG_DATA_HOME" ]]; then
+    prefix=~/.local/share
+else
+    prefix="$XDG_DATA_HOME"
+fi
+
+mkdir -p $prefix/kservices5/krunner/dbusplugins/
+mkdir -p $prefix/dbus-1/services/
+
+cp plasma-runner-%{APPNAMELC}.desktop $prefix/kservices5/krunner/dbusplugins/
+sed "s|%{PROJECTDIR}/%{APPNAMELC}.py|${PWD}/%{APPNAMELC}.py|" "org.kde.%{APPNAMELC}.service" > $prefix/dbus-1/services/org.kde.%{APPNAMELC}.service
 
 kquitapp5 krunner
 
--- templates/runnerpython/uninstall.sh
+++ templates/runnerpython/uninstall.sh
@@ -3,7 +3,13 @@
 # Exit if something fails
 set -e
 
-rm ~/.local/share/kservices5/krunner/dbusplugins/plasma-runner-%{APPNAMELC}.desktop
-rm ~/.local/share/dbus-1/services/org.kde.%{APPNAMELC}.service
+if [[ -z "$XDG_DATA_HOME" ]]; then
+    prefix=~/.local/share
+else
+    prefix="$XDG_DATA_HOME"
+fi
+
+rm $prefix/kservices5/krunner/dbusplugins/plasma-runner-%{APPNAMELC}.desktop
+rm $prefix/dbus-1/services/org.kde.%{APPNAMELC}.service
 kquitapp5 krunner
 
