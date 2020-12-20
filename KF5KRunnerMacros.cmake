cd7e7c90af87141696a38d578d2585704c181e78
Alexander Lohnau <alexander.lohnau@gmx.de>
2020-12-20 15:45:11 +0100

    Add header and CMake configuration to help writing tests

---
+++ KF5KRunnerMacros.cmake
@@ -0,0 +1,39 @@
+#.rst:
+# KF5KRunnerMacros
+# ---------------------------
+#
+# This module provides the ``configure_krunner_test`` function which takes the test- and runner target as a parameter.
+# This will add the compile definitions for the AbstractRunnerTest header.
+# In case of DBus runners the DESKTOP_FILE parameter must be set. This is required for loading the runner from the
+# metadata file.
+
+# SPDX-FileCopyrightText: (C) 2020 Alexander Lohnau <alexander.lohnau@gmx.de>
+# SPDX-License-Identifier: BSD-2-Clause
+
+function(configure_krunner_test TEST_TARGET RUNNER_TARGET)
+    include(CMakeParseArguments)
+    set(options)
+    set(oneValueArgs DESKTOP_FILE)
+    set(multiValueArgs)
+    cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    get_target_property(target_type ${RUNNER_TARGET} TYPE)
+    if(target_type STREQUAL "EXECUTABLE")
+        if(NOT ARGS_DESKTOP_FILE)
+            message(FATAL_ERROR "In case of a dbus runner the DESKTOP_FILE must be provided")
+        endif()
+        target_compile_definitions(${TEST_TARGET}
+                PRIVATE
+                KRUNNER_DBUS_RUNNER_TESTING=1
+                KRUNNER_TEST_DBUS_EXECUTABLE="$<TARGET_FILE:${RUNNER_TARGET}>"
+                KRUNNER_TEST_DESKTOP_FILE="${ARGS_DESKTOP_FILE}"
+                )
+    else()
+        target_compile_definitions(${TEST_TARGET}
+                PRIVATE
+                KRUNNER_DBUS_RUNNER_TESTING=0
+                KRUNNER_TEST_RUNNER_PLUGIN_DIR="$<TARGET_FILE_DIR:${RUNNER_TARGET}>"
+                KRUNNER_TEST_RUNNER_PLUGIN_NAME="$<TARGET_FILE_NAME:${RUNNER_TARGET}>"
+                )
+    endif()
+    add_dependencies(${TEST_TARGET} ${RUNNER_TARGET})
+endfunction()
---
+++ src/abstractrunnertest.h
@@ -0,0 +1,116 @@
+/*
+    SPDX-FileCopyrightText: 2020 Alexander Lohnau <alexander.lohnau@gmx.de>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <QStandardPaths>
+#include <KPluginLoader>
+#include <KPluginMetaData>
+#include <KRunner/RunnerManager>
+#include <KRunner/AbstractRunner>
+
+#include <QTest>
+#include <QSignalSpy>
+#if KRUNNER_DBUS_RUNNER_TESTING
+#include <QDBusServiceWatcher>
+#include <QDBusConnection>
+#endif
+
+namespace
+{
+/**
+ * This class provides a basic structure for a runner test.
+ * The compile definitions should be configured using the `add_krunner_test` cmake macro
+ * @since 5.80
+ */
+class AbstractRunnerTest : public QObject
+{
+public:
+    using QObject::QObject;
+    std::unique_ptr<Plasma::RunnerManager> manager = nullptr;
+    Plasma::AbstractRunner *runner = nullptr;
+
+    /**
+     * Load the runner and set the manager and runner properties.
+     */
+    void initProperties()
+    {
+        setenv("LC_ALL", "C.utf-8", 1);
+        manager.reset(new Plasma::RunnerManager());
+
+#if KRUNNER_DBUS_RUNNER_TESTING
+        auto md = KPluginMetaData::fromDesktopFile(QStringLiteral(KRUNNER_TEST_DESKTOP_FILE),
+                                                   {QStringLiteral("plasma-runner.desktop")});
+        QVERIFY(md.isValid());
+        manager->loadRunner(md);
+#else
+        auto pluginMetaDatas = KPluginLoader::findPluginsById(QStringLiteral(KRUNNER_TEST_RUNNER_PLUGIN_DIR),
+                                                              QFileInfo(QStringLiteral(KRUNNER_TEST_RUNNER_PLUGIN_NAME)).baseName());
+        QCOMPARE(pluginMetaDatas.count(), 1);
+        KPluginMetaData runnerMetadata = pluginMetaDatas.first();
+
+        // Set internal variables
+        manager->loadRunner(runnerMetadata);
+#endif
+        QCOMPARE(manager->runners().count(), 1);
+        runner = manager->runners().constFirst();
+
+        // Just make sure all went well
+        QVERIFY(runner);
+    }
+
+    /**
+     * Launch a query and wait for the RunnerManager to finish
+     * @param query
+     * @param runnerName
+     */
+    void launchQuery(const QString &query, const QString &runnerName = QString())
+    {
+        QSignalSpy spy(manager.get(), &Plasma::RunnerManager::queryFinished);
+        manager->launchQuery(query, runnerName);
+        QVERIFY2(spy.wait(), "RunnerManager did not emit the queryFinished signal");
+    }
+#if KRUNNER_DBUS_RUNNER_TESTING
+    /**
+     * Launch the configured DBus executable with the given arguments and wait for the process to be started.
+     * @param args
+     * @param waitForService Wait for this service to be registered, this will default to the service from the metadata
+     * @return Process that was successfully started
+     */
+    QProcess *startDBusRunnerProcess(const QStringList &args = {}, const QString waitForService = QString())
+    {
+        QProcess *process = new QProcess();
+        auto md = KPluginMetaData::fromDesktopFile(QStringLiteral(KRUNNER_TEST_DESKTOP_FILE), {QStringLiteral("plasma-runner.desktop")});
+        QString serviceToWatch = waitForService;
+        if (serviceToWatch.isEmpty()) {
+            serviceToWatch = md.value(QStringLiteral("X-Plasma-DBusRunner-Service"));
+        }
+        QDBusServiceWatcher watcher(serviceToWatch, QDBusConnection::sessionBus(), QDBusServiceWatcher::WatchForRegistration);
+
+        QEventLoop loop;
+        connect(&watcher, &QDBusServiceWatcher::serviceRegistered, &loop, &QEventLoop::quit);
+        process->start(QStringLiteral(KRUNNER_TEST_DBUS_EXECUTABLE), args);
+        loop.exec();
+
+        Q_ASSERT(process->state() == QProcess::ProcessState::Running);
+        m_runningProcesses << process;
+        return process;
+    }
+
+    /**
+     * Kill all processes that got started with the startDBusRunnerProcess
+     */
+    void killRunningDBusProcesses()
+    {
+        for (auto &process : qAsConst(m_runningProcesses)) {
+            process->kill();
+            QVERIFY(process->waitForFinished());
+        }
+        qDeleteAll(m_runningProcesses);
+        m_runningProcesses.clear();
+    }
+private:
+    QList<QProcess *> m_runningProcesses;
+#endif
+};
+}
