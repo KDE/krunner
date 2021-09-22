f709023e841854e83f59acea36a585e0eb61c4e5
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-22 18:38:41 +0200

    Mark non-copyrightable files as CC0-1.0

--- ExtraDesktop.sh
+++ ExtraDesktop.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: CC0-1.0
+
 #This file outputs in a separate line each file with a .desktop syntax
 #that needs to be translated but has a non .desktop extension
 find -name \*.kdevtemplate -print
--- src/config.h.cmake
+++ src/config.h.cmake
@@ -1,1 +1,4 @@
+# SPDX-FileCopyrightText: Alexander Lohnau <alexander.lohnau@gmx.de>
+# SPDX-License-Identifier: CC0-1.0
+
 #cmakedefine HAVE_KACTIVITIES
