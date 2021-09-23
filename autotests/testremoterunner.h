9b03156b490fab7760afff98449071164fbcc681
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-23 21:50:19 +0200

    Add SPDX license info to testremoterunner.h
    The cpp file contained a license while the header did not

--- autotests/testremoterunner.h
+++ autotests/testremoterunner.h
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2017 David Edmundson <davidedmundson@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
 #pragma once
 
 #include "../src/dbusutils_p.h"
