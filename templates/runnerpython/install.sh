5e98172c347f7aa9e1546c101207a9cefc635269
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-29 20:34:49 +0200

    GIT_SILENT Adjust install location of dbus python runner metadata file

--- templates/runnerpython/install.sh
+++ templates/runnerpython/install.sh
@@ -13,7 +13,7 @@ fi
 mkdir -p $prefix/kservices5/krunner/dbusplugins/
 mkdir -p $prefix/dbus-1/services/
 
-cp plasma-runner-%{APPNAMELC}.desktop $prefix/kservices5/krunner/dbusplugins/
+cp plasma-runner-%{APPNAMELC}.desktop $prefix/krunner/dbusplugins/
 sed "s|%{PROJECTDIR}/%{APPNAMELC}.py|${PWD}/%{APPNAMELC}.py|" "org.kde.%{APPNAMELC}.service" > $prefix/dbus-1/services/org.kde.%{APPNAMELC}.service
 
 kquitapp5 krunner
