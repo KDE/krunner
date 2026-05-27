KDE/krunner

commit ab46552eabbb71f6459458faae1a12bfbb742506

Emit historyChanged() when history changed
Supports https://invent.kde.org/plasma/plasma-workspace/-/merge_requests/5854

Right now `history` is a property, but it's not `NOTIFY`able, limiting how
it can be used from QML. Emit some signals when it changes.

CCBUG: 507588

src/CMakeLists.txt
@@ -59,6 +59,7 @@ target_link_libraries(KF6Runner
     PUBLIC
         Qt6::Core
         KF6::CoreAddons # KPluginFactory
+        KF6::ConfigCore
     PRIVATE
         Qt6::DBus
         Qt6::Gui

src/runnermanager.cpp
@@ -346,6 +346,7 @@ public:
     {
         stateData.group(QStringLiteral("History")).writeEntry(historyEnvironmentIdentifier, historyEntries, KConfig::Notify);
         stateData.sync();
+        Q_EMIT q->historyChanged();
     }
 
     inline QStringList readHistoryForCurrentEnv()
@@ -393,6 +394,10 @@ RunnerManager::RunnerManager(QObject *parent)
     d = std::make_unique<RunnerManagerPrivate>(configPtr->group(QStringLiteral("Plugins")),
                                                defaultStatePtr->group(QStringLiteral("PlasmaRunnerManager")),
                                                this);
+    m_stateWatcher = KConfigWatcher::create(defaultStatePtr);
+    connect(m_stateWatcher.data(), &KConfigWatcher::configChanged, this, [this]() {
+        Q_EMIT historyChanged();
+    });
 }
 
 RunnerManager::~RunnerManager()

src/runnermanager.h
@@ -13,6 +13,7 @@
 #include <QList>
 #include <QObject>
 
+#include <KConfigWatcher>
 #include <KPluginMetaData>
 
 #include "abstractrunner.h"
@@ -48,7 +49,7 @@ class KRUNNER_EXPORT RunnerManager : public QObject
     /*!
      * \property KRunner::RunnerManager::history
      */
-    Q_PROPERTY(QStringList history READ history)
+    Q_PROPERTY(QStringList history READ history NOTIFY historyChanged)
 
     /*!
      * \property KRunner::RunnerManager::querying
@@ -285,12 +286,20 @@ Q_SIGNALS:
      */
     void historyEnabledChanged();
 
+    /*!
+     * Emitted when the history has changed
+     * \since 6.21
+     */
+
+    void historyChanged();
+
 private:
     // exported for dbusrunnertest
     KPluginMetaData convertDBusRunnerToJson(const QString &filename) const;
     KRUNNER_NO_EXPORT Q_INVOKABLE void onMatchesChanged();
 
     std::unique_ptr<RunnerManagerPrivate> d;
+    KConfigWatcher::Ptr m_stateWatcher;
 
     friend class RunnerManagerPrivate;
     friend AbstractRunnerTest;

commit 4847bc14acab49fd1b403043340d2d69afac69eb

RunnerManager: move spurious member into pimpl class
ABI break, restoring ABI broken in ab46552eabbb71f6459458faae1a12bfbb742506

src/CMakeLists.txt
@@ -59,7 +59,6 @@ target_link_libraries(KF6Runner
     PUBLIC
         Qt6::Core
         KF6::CoreAddons # KPluginFactory
-        KF6::ConfigCore
     PRIVATE
         Qt6::DBus
         Qt6::Gui

src/runnermanager.cpp
@@ -35,7 +35,10 @@ namespace KRunner
 class RunnerManagerPrivate
 {
 public:
-    RunnerManagerPrivate(const KConfigGroup &configurationGroup, const KConfigGroup &stateConfigGroup, RunnerManager *parent)
+    RunnerManagerPrivate(const KConfigGroup &configurationGroup,
+                         const KConfigGroup &stateConfigGroup,
+                         const KSharedConfig::Ptr &defaultStatePtr,
+                         RunnerManager *parent)
         : q(parent)
         , context(parent)
         , pluginConf(configurationGroup)
@@ -54,6 +57,13 @@ public:
         QObject::connect(q, &RunnerManager::matchesChanged, q, [&] {
             lastMatchChangeSignalled.restart();
         });
+
+        if (defaultStatePtr) {
+            defaultStateWatcher = KConfigWatcher::create(defaultStatePtr);
+            QObject::connect(defaultStateWatcher.data(), &KConfigWatcher::configChanged, q, [this]() {
+                Q_EMIT q->historyChanged();
+            });
+        }
     }
 
     void scheduleMatchesChanged()
@@ -373,11 +383,12 @@ public:
     KConfigGroup pluginConf;
     KConfigGroup stateData;
     QSet<QString> disabledRunnerIds; // Runners that are disabled but were loaded as single runners
+    KConfigWatcher::Ptr defaultStateWatcher;
 };
 
 RunnerManager::RunnerManager(const KConfigGroup &pluginConfigGroup, const KConfigGroup &stateConfigGroup, QObject *parent)
     : QObject(parent)
-    , d(new RunnerManagerPrivate(pluginConfigGroup, stateConfigGroup, this))
+    , d(new RunnerManagerPrivate(pluginConfigGroup, stateConfigGroup, KSharedConfig::Ptr(), this)) // TODO: need to watch stateconfig also here?
 {
     Q_ASSERT(pluginConfigGroup.isValid());
     Q_ASSERT(stateConfigGroup.isValid());
@@ -390,11 +401,8 @@ RunnerManager::RunnerManager(QObject *parent)
     auto configPtr = KSharedConfig::openConfig(QStringLiteral("krunnerrc"), KConfig::NoGlobals);
     d = std::make_unique<RunnerManagerPrivate>(configPtr->group(QStringLiteral("Plugins")),
                                                defaultStatePtr->group(QStringLiteral("PlasmaRunnerManager")),
+                                               defaultStatePtr,
                                                this);
-    m_stateWatcher = KConfigWatcher::create(defaultStatePtr);
-    connect(m_stateWatcher.data(), &KConfigWatcher::configChanged, this, [this]() {
-        Q_EMIT historyChanged();
-    });
 }
 
 RunnerManager::~RunnerManager()

src/runnermanager.h
@@ -13,7 +13,6 @@
 #include <QList>
 #include <QObject>
 
-#include <KConfigWatcher>
 #include <KPluginMetaData>
 
 #include "abstractrunner.h"
@@ -299,7 +298,6 @@ private:
     KRUNNER_NO_EXPORT Q_INVOKABLE void onMatchesChanged();
 
     std::unique_ptr<RunnerManagerPrivate> d;
-    KConfigWatcher::Ptr m_stateWatcher;
 
     friend class RunnerManagerPrivate;
     friend AbstractRunnerTest;